402174537f49b7bcc86a32ffae8d6cc20dcbb547
JMkase2013 <jmkase2013@gmail.com>
2025-12-15 22:54:39 -0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,15 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { StoresModule } from './stores/stores.module';
 import { ProductsModule } from './products/products.module';
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule, AuthModule],
+})
+export class AppModule {}
+
 
 @Module({
   imports: [
