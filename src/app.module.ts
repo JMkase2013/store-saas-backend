2e68761e2d6aeb65d6ea5509655224813819028f
JMkase2013 <jmkase2013@gmail.com>
2025-12-15 22:19:17 -0600

    Create app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { StoresModule } from './stores/stores.module';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+    StoresModule,
+    ProductsModule,
+  ],
+})
+export class AppModule {}
